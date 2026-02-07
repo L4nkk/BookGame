f89477e8952974d7f684673361486ef2544dc591
jose <jose.mantyla@tuni.com>
2026-02-07 23:11:36 +0200

    Prefab for typing game and color for words

--- Assets/Scripts/BookAI.cs
+++ Assets/Scripts/BookAI.cs
@@ -3,118 +3,132 @@ using UnityEngine;
 
 public class BookAI : MonoBehaviour
 {
-    [SerializeField] private WordChecker _wordChecker;
-    [SerializeField] private TextInput _textInput;
-    [SerializeField] private FaceController _faceController;
-    [SerializeField] private GameObject _loveEffect;
-    [SerializeField] private GameObject _dislikeEffect;
-    [SerializeField] private AudioSource _audioSource;
-    [SerializeField] private AudioClip[] _positiveClips;
-    [SerializeField] private AudioClip[] _negativeClips;
-    [SerializeField] private AudioClip[] _superPositiveClips;
-    [SerializeField] private AudioClip[] _superNegativeClips;
-    [SerializeField] private float _effectDuration = 2f;
+	[SerializeField] private WordChecker _wordChecker;
+	[SerializeField] private TextInput _textInput;
+	[SerializeField] private FaceController _faceController;
+	[SerializeField] private GameObject _loveEffect;
+	[SerializeField] private GameObject _dislikeEffect;
+	[SerializeField] private AudioSource _audioSource;
+	[SerializeField] private AudioClip[] _positiveClips;
+	[SerializeField] private AudioClip[] _negativeClips;
+	[SerializeField] private AudioClip[] _superPositiveClips;
+	[SerializeField] private AudioClip[] _superNegativeClips;
+	[SerializeField] private AudioClip[] _neutralClips;
+	[SerializeField] private AudioClip[] _onClickClips;
+	[SerializeField] private float _effectDuration = 2f;
 
-    private void Start()
-    {
-        if (_wordChecker == null)
-        {
-            _wordChecker = GetComponent<WordChecker>();
-        }
+	private void Start()
+	{
+		if (_wordChecker == null)
+		{
+			_wordChecker = GetComponent<WordChecker>();
+		}
 
-        if (_textInput == null)
-        {
-            _textInput = FindFirstObjectByType<TextInput>();
-        }
+		if (_textInput == null)
+		{
+			_textInput = FindFirstObjectByType<TextInput>();
+		}
 
-        if (_faceController == null)
-        {
-            _faceController = GetComponent<FaceController>();
-        }
+		if (_faceController == null)
+		{
+			_faceController = GetComponent<FaceController>();
+		}
 
-        if (_audioSource == null)
-        {
-            _audioSource = GetComponent<AudioSource>();
-        }
-    }
+		if (_audioSource == null)
+		{
+			_audioSource = GetComponent<AudioSource>();
+		}
+	}
 
-    private void OnEnable()
-    {
-        _wordChecker.WordChecked += ReactToPlayerInput;
-    }
+	private void OnEnable()
+	{
+	}
 
-    private void OnDisable()
-    {
-        _wordChecker.WordChecked -= ReactToPlayerInput;
-        _textInput.TextEntered -= OnTextEntered;
-    }
+	private void OnDisable()
+	{
+		_textInput.TextEntered -= OnTextEntered;
+	}
 
-    public void StartInteraction()
-    {
-        _textInput.TextEntered += OnTextEntered;
-    }
+	public void StartInteraction()
+	{
+		_textInput.TextEntered += OnTextEntered;
+	}
 
-    public void EndInteraction()
-    {
-        _textInput.TextEntered -= OnTextEntered;
-    }
+	public void EndInteraction()
+	{
+		_textInput.TextEntered -= OnTextEntered;
+	}
 
-    private void OnTextEntered(string text)
-    {
-        _wordChecker.CheckWord(text);
-    }
+	private void OnTextEntered(string text)
+	{
+		WordValue wordValue = _wordChecker.CheckWord(text);
+		_textInput.UpdateFoundWordsText(text, wordValue);
+		ReactToPlayerInput(wordValue);
+	}
 
-    private void ReactToPlayerInput(WordValue wordValue)
-    {
-        switch (wordValue)
-        {
-            case WordValue.Positive:
-                // Do positive things
-                Debug.Log("Positive word detected!");
-                _faceController.SetExpression(_faceController.happy);
-                StartCoroutine(EffectCoroutine(_loveEffect));
-                _audioSource.clip = _positiveClips[Random.Range(0, _positiveClips.Length)];
-                _audioSource.Play();
-                break;
-            case WordValue.SuperPositive:
-                // Do super positive things
-                Debug.Log("Super positive word detected!");
-                _faceController.SetExpression(_faceController.happy);
-                StartCoroutine(EffectCoroutine(_loveEffect));
-                _audioSource.clip = _superPositiveClips[Random.Range(0, _superPositiveClips.Length)];
-                _audioSource.Play();
-                break;
-            case WordValue.Negative:
-                // Do negative things
-                Debug.Log("Negative word detected!");
-                _faceController.SetExpression(_faceController.sad);
-                StartCoroutine(EffectCoroutine(_dislikeEffect));
-                _audioSource.clip = _negativeClips[Random.Range(0, _negativeClips.Length)];
-                _audioSource.Play();
-                break;
-            case WordValue.SuperNegative:
-                // Do super negative things
-                Debug.Log("Super negative word detected!");
-                _faceController.SetExpression(_faceController.angry);
-                StartCoroutine(EffectCoroutine(_dislikeEffect));
-                _audioSource.clip = _superNegativeClips[Random.Range(0, _superNegativeClips.Length)];
-                _audioSource.Play();
-                break;
-            default:
-                // Word not recognized
-                Debug.Log("Word not recognized.");
-                break;
-        }
-    }
+	private void ReactToPlayerInput(WordValue wordValue)
+	{
+		switch (wordValue)
+		{
+			case WordValue.Positive:
+				// Do positive things
+				Debug.Log("Positive word detected!");
+				_faceController.SetExpression(_faceController.happy);
+				StartCoroutine(EffectCoroutine(_loveEffect));
+				_audioSource.clip = _positiveClips[Random.Range(0, _positiveClips.Length)];
+				_audioSource.Play();
+				break;
+			case WordValue.SuperPositive:
+				// Do super positive things
+				Debug.Log("Super positive word detected!");
+				_faceController.SetExpression(_faceController.happy);
+				StartCoroutine(EffectCoroutine(_loveEffect));
+				_audioSource.clip = _superPositiveClips[Random.Range(0, _superPositiveClips.Length)];
+				_audioSource.Play();
+				break;
+			case WordValue.Negative:
+				// Do negative things
+				Debug.Log("Negative word detected!");
+				_faceController.SetExpression(_faceController.sad);
+				StartCoroutine(EffectCoroutine(_dislikeEffect));
+				_audioSource.clip = _negativeClips[Random.Range(0, _negativeClips.Length)];
+				_audioSource.Play();
+				break;
+			case WordValue.SuperNegative:
+				// Do super negative things
+				Debug.Log("Super negative word detected!");
+				_faceController.SetExpression(_faceController.angry);
+				StartCoroutine(EffectCoroutine(_dislikeEffect));
+				_audioSource.clip = _superNegativeClips[Random.Range(0, _superNegativeClips.Length)];
+				_audioSource.Play();
+				break;
+			default:
+				// Word not recognized
+				Debug.Log("Word not recognized.");
+				_faceController.SetExpression(_faceController.neutral);
+				StartCoroutine(EffectCoroutine(null));
+				_audioSource.clip = _neutralClips[Random.Range(0, _neutralClips.Length)];
+				_audioSource.Play();
+				break;
+		}
+	}
 
-    private IEnumerator EffectCoroutine(GameObject effect)
-    {
-        foreach (Transform child in effect.transform)
-        {
-            child.gameObject.GetComponent<ParticleSystem>().Play();
-        }
-        _faceController.isTalking = true;
-        yield return new WaitForSeconds(_effectDuration);
-        _faceController.isTalking = false;
-    }
+	private IEnumerator EffectCoroutine(GameObject effect)
+	{
+		if (effect == null)
+		{
+			_faceController.isTalking = true;
+			yield return new WaitForSeconds(_effectDuration);
+			_faceController.isTalking = false;
+			yield break;
+		}
+
+		foreach (Transform child in effect.transform)
+		{
+			child.gameObject.GetComponent<ParticleSystem>().Play();
+		}
+		_faceController.isTalking = true;
+		yield return new WaitForSeconds(_effectDuration);
+		_faceController.isTalking = false;
+	}
 }
--- Assets/Scripts/TextInput.cs
+++ Assets/Scripts/TextInput.cs
@@ -2,31 +2,68 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using System;
+using Unity.VisualScripting;
 
 public class TextInput : MonoBehaviour
 {
-    [SerializeField] private TMP_InputField _inputField;
-    private string _inputText;
+	[SerializeField] private TMP_InputField _inputField;
+	private string _inputText;
+	[SerializeField] private AudioSource _audioSource;
+	[SerializeField] private TextMeshProUGUI _foundWordsText;
+	[SerializeField] private Color _positiveColor = Color.green;
+	[SerializeField] private Color _negativeColor = Color.red;
 
-    public event Action<string> TextEntered;
+	public event Action<string> TextEntered;
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            EnterButtonPressed();
-        }
-    }
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			EnterButtonPressed();
+		}
+	}
 
-    public void EnterButtonPressed()
-    {
-        _inputText = _inputField.text.Trim().ToLower();
-        Debug.Log(_inputText);
-        
-        if (TextEntered != null)
-        {
-            TextEntered.Invoke(_inputText);
-        }
-        _inputField.text = "";
-    }
+	public void EnterButtonPressed()
+	{
+		_inputText = _inputField.text.Trim().ToLower();
+		Debug.Log(_inputText);
+
+		if (TextEntered != null)
+		{
+			TextEntered.Invoke(_inputText);
+		}
+		_inputField.text = "";
+	}
+
+	public void PlayInputSound()
+	{
+		if (_audioSource != null)
+		{
+			_audioSource.Play();
+		}
+	}
+
+	public void UpdateFoundWordsText(string word, WordValue wordValue)
+	{
+		string cleanedWord = word.Trim().ToLower();
+		if (wordValue != WordValue.None && _foundWordsText != null)
+		{
+			// Add the found word the display with a color based on its value
+			switch (wordValue)
+			{
+				case WordValue.Positive:
+					_foundWordsText.text += $"<mark={"#" + _positiveColor.ToHexString()}>{cleanedWord}</mark> ";
+					break;
+				case WordValue.SuperPositive:
+					_foundWordsText.text += $"<mark={"#" + _positiveColor.ToHexString()}><b>{cleanedWord}</b></mark> ";
+					break;
+				case WordValue.Negative:
+					_foundWordsText.text += $"<mark={"#" + _negativeColor.ToHexString()}>{cleanedWord}</mark> ";
+					break;
+				case WordValue.SuperNegative:
+					_foundWordsText.text += $"<mark={"#" + _negativeColor.ToHexString()}><b>{cleanedWord}</b></mark> ";
+					break;
+			}
+		}
+	}
 }
